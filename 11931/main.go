e6dbec641425169126b61d14a284296736dd9626
Youngjae Shin (신영재) <sdhkygg@gmail.com>
2023-07-03 01:53:23 +0900

    11931: standard library

---
+++ 11931/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	var N int
+	fmt.Scan(&N)
+
+	numbers := make([]int, N)
+	for i := 0; i < N; i++ {
+		fmt.Scan(&numbers[i])
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(numbers)))
+
+	// Print numbers
+	for _, number := range numbers {
+		fmt.Println(number)
+	}
+}
--- 11931/main.py
+++ 11931/main.py
@@ -1,15 +1,4 @@
 n = int(input())
-n_list = []
-for i in range(0, n):
-    a = int(input())
-    n_list.append(a)
-new_list = [n_list.pop(0), -1000000]
-for num in n_list:
-    before_num = 1000000
-    for j in range(0, len(new_list)):
-        num_2 = new_list[j]
-        if num_2 < num < before_num:
-            new_list.insert(j, num)
-            break
-        before_num = num_2
-print(new_list[:-1])
+numbers = [int(input()) for _ in range(n)]
+numbers.sort(reverse=True)
+print('\n'.join(str(number) for number in numbers))
