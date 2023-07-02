90701b80bb2afd31b8ac1532f7b3b3fa3a102512
Youngjae Shin (신영재) <sdhkygg@gmail.com>
2023-07-03 01:29:14 +0900

    11931

--- 11931/main.py
+++ 11931/main.py
@@ -3,13 +3,13 @@ n_list = []
 for i in range(0, n):
     a = int(input())
     n_list.append(a)
-new_list = [n_list.pop(0), -1000000]
+new_list = [max(n_list), min(n_list)]
 for num in n_list:
-    before_num = 1000000
+    before_num = new_list[0]
     for j in range(0, len(new_list)):
         num_2 = new_list[j]
         if num_2 < num < before_num:
             new_list.insert(j, num)
             break
         before_num = num_2
-print(new_list[:-1])
+print(new_list)
